1418784cae20cf55c0aa03816f1de600c89d4ad1
Astitva <astitvas999@gmail.com>
2026-04-16 07:44:58 +0530

    fix cors syntax

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="JUSTIS API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nyay-justis.vercel.app"]
+    allow_origins=["https://nyay-justis.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
